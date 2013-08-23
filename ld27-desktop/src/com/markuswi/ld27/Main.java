486b8109ede86a32114a2534c3041ee9b6c4cbed
markus-wi <emveyh@googlemail.com>
2013-08-24 00:56:25 +0200

    initial commit

---
+++ ld27-desktop/src/com/markuswi/ld27/Main.java
@@ -0,0 +1,16 @@
+package com.markuswi.ld27;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "ld27";
+		cfg.useGL20 = false;
+		cfg.width = 800;
+		cfg.height = 480;
+
+		new LwjglApplication(new GdxGame(), cfg);
+	}
+}
